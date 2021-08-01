f6aa586457528234b183459e26d18ea8e1a601c7
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-08-01 15:26:49 +0900

    done 2; done 1 retake

--- src/audio/extract-sounds.py
+++ src/audio/extract-sounds.py
@@ -7,13 +7,38 @@ import tgt as textgrid  # tgt is ambiguous: target--textgrid
 # %%
 # 前提:
 # wavとtextgridは同じ名前で保存されている
-pid = 1
-target = f"{pid}-edit"
 ignore = "silent"
 tier_name = "silences"
-tgt_path = f'{target}.TextGrid'
 output_dir = "output"
+#%%
+pid = 1
+target = f"{pid}-edit"
+tgt_path = f'{target}.TextGrid'
+tier = textgrid.io.read_textgrid(tgt_path).get_tier_by_name(tier_name)
+tgt_intervals = filter(lambda t: t.text != ignore, tier)
+snd = parselmouth.Sound(f"{target}.wav")
+for interval in tgt_intervals:
+    snd_part = snd.extract_part(from_time=interval.start_time,
+                                to_time=interval.end_time,
+                                preserve_times=False)
+    snd_part.save(f"{output_dir}/" + interval.text + f"-{pid}.wav", 'WAV')
 
+#%%
+pid = 1
+target = f"{pid}-retake" # 上書きする
+tgt_path = f'{target}.TextGrid'
+tier = textgrid.io.read_textgrid(tgt_path).get_tier_by_name(tier_name)
+tgt_intervals = filter(lambda t: t.text != ignore, tier)
+snd = parselmouth.Sound(f"{target}.wav")
+for interval in tgt_intervals:
+    snd_part = snd.extract_part(from_time=interval.start_time,
+                                to_time=interval.end_time,
+                                preserve_times=False)
+    snd_part.save(f"{output_dir}/" + interval.text + f"-{pid}.wav", 'WAV')
+#%%
+pid = 2
+target = f"{pid}-edit"
+tgt_path = f'{target}.TextGrid'
 tier = textgrid.io.read_textgrid(tgt_path).get_tier_by_name(tier_name)
 tgt_intervals = filter(lambda t: t.text != ignore, tier)
 snd = parselmouth.Sound(f"{target}.wav")
@@ -24,3 +49,4 @@ for interval in tgt_intervals:
     snd_part.save(f"{output_dir}/" + interval.text + f"-{pid}.wav", 'WAV')
 
 # TODO: do the same thing for retakes
+# %%
