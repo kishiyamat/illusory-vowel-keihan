dcc6961df9c6b50ab575f69b676c36b0b833d849
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-08-03 12:42:34 +0900

    こまかな指示の修正

--- src/js/production.js
+++ src/js/production.js
@@ -1,22 +1,23 @@
 // PRODUCTION EXPERIMENT
 var production_instructions_practice = {
   type: "html-keyboard-response",
+  choices: [' '],
   stimulus: `
-    <p>スペースキーを押すと練習課題に移ってしまうので、
-    <u>以下のお願いを全て読んでから</u>キーを押してください。</p>
-    <p>この産出実験では、一番上に表示される単語を
+    <p>　この産出実験では、一番上に表示される単語を
     大きな声、標準語で3回ずつ読み上げていただきます。
     マイクへのアクセスが要求さればた場合は許可し、
-    静かな環境での実施をお願いいたします。</p>
-    <p>スペースキーを押すと練習課題を2問呈示いたします。</p>
+    静かな環境での実施をお願いいたします。
+    スペースキーを押すと練習課題を2問呈示いたします。
+    </p>
     `,
 };
 
 var record = {
     type: 'html-audio-response_modified',
     stimulus: jsPsych.timelineVariable('read'),
-    prompt: `<p>上の単語を大きな声で標準語で3回読み上げたらスペースキーを押してください。</p>
-    <p>再生ボタンを押して確認し、上手く録音できた場合は「次へ進む」、
+    prompt: `
+    <p>　上の単語を大きな声で標準語で3回読み上げたらスペースキーを押してください。
+    再生ボタンを押して確認し、上手く録音できた場合は「次へ進む」、
     上手く録音できなかった場合は「やり直す」を押してください。</p>
     <p> ※丸が赤で満たされている時は録音中です。 </p>
     `, 
@@ -42,8 +43,9 @@ var production_practice = {
 
 var production_instructions = {
   type: "html-keyboard-response",
+  choices: [' '],
   stimulus: `
-    <p>以上で練習は終わりです。スペースキーを押すと2セットの本番の産出課題に移ります。</p>
+    <p>　以上で練習は終わりです。スペースキーを押すと2セットの本番の産出課題に移ります。</p>
     `,
 };
 
@@ -57,8 +59,7 @@ var production_end = {
   type: "html-keyboard-response",
   choices: [' '],
   stimulus: `
-    <p>
-    産出課題はこれで終了となります。スペースキーを押して次に進んでください。
+    <p>　産出課題はこれで終了となります。スペースキーを押して次に進んでください。
     </p>
   `,
 };
