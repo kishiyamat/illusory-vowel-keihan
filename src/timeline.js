69b1898b61d691967f85c5fb1c756b3dd24d9b32
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-08-04 12:49:22 +0900

    finalize

--- src/timeline.js
+++ src/timeline.js
@@ -10,19 +10,19 @@ timeline.push(informed_consent);
 timeline.push(form_trial);
 timeline.push(welcome);
 
-// // produciotn
-// timeline.push(production_instructions_practice);
-// timeline.push(production_practice);
-// timeline.push(production_instructions);
-// timeline.push(production);
-// timeline.push(production_end);
-// 
-// // produciotn
-// timeline.push(axb_instructions_practice);
-// timeline.push(axb_practice);
-// timeline.push(axb_instructions);
-// timeline.push(axb);
-// timeline.push(axb_end);
+// produciotn
+timeline.push(production_instructions_practice);
+timeline.push(production_practice);
+timeline.push(production_instructions);
+timeline.push(production);
+timeline.push(production_end);
+
+// produciotn
+timeline.push(axb_instructions_practice);
+timeline.push(axb_practice);
+timeline.push(axb_instructions);
+timeline.push(axb);
+timeline.push(axb_end);
 
 // categorize
 timeline.push(cat_instructions_practice);
