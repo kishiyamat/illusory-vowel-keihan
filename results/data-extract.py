1c08f421cf6aca7eaf62003e3c8db9586c180149
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-09-17 06:55:29 +0900

    subject_id がないケースも取得

--- results/data-extract.py
+++ results/data-extract.py
@@ -4,16 +4,23 @@ import pandas as pd
 import base64
 from ffmpy import FFmpeg
 
-N_TRIALS = 4
+N_TRIALS = 9
+# FIXME: ここの名前を考え直す
 csv_path = "illusory-vowel-keihan.csv"
+# 事前に webm や wav の中身は空にしないといけない
 results = pd.read_csv(csv_path)
 # run_id ベースで分析をすればかぶらない
 #%%
 # run_id -- subject id
 columns = ["run_id", "trial_type", "response"]
 subject_id_trial = "survey-html-form"
+# TODO: あとでspan_tokyoがないトライアルの対処を考える
+# TODO: あとでspan_tokyoの対処を考える
 run_id_subj_id = results[columns].query(f"trial_type == '{subject_id_trial}'")
+run_id_subj_id = run_id_subj_id[run_id_subj_id['response'].str.contains("subject_id")]
+print(run_id_subj_id)
 run_id_subj_id["response"] = run_id_subj_id.response.apply(lambda res: int(eval(res)["subject_id"]))
+print(len(run_id_subj_id))
 assert len(run_id_subj_id) == N_TRIALS
 #%%
 # run_id -- wav
