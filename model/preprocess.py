80fb1db140dfb68f139add85fdd41918e1c5e6d7
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-10-14 05:20:02 +0900

    refactor

--- model/preprocess.py
+++ model/preprocess.py
@@ -1,92 +1,114 @@
 # %%
-import optuna
-from numpy.lib.function_base import percentile
-from scipy.signal import find_peaks
 from pathlib import Path
-from typing import List
 
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
+import optuna
 import pandas as pd
 import soundfile as sf
-from librosa.core.audio import tone
-
+from scipy.signal import find_peaks
 # %%
-csv_name = "axb_list.csv"
+# Get Train/Test dataset
 project_dir = Path("../")
+csv_name = "axb_list.csv"
 list_path = project_dir/"src/list"/csv_name
-target_cols = ["task", "item_id", "type", "a", "x",
-               "b", "correct", "item_a", "item_x", "item_b"]
-csv_df = pd.read_csv(list_path)[target_cols]
-tone_df = csv_df.query("type=='filler'")
-train_wav_list = list(filter(lambda s: s.count("_") == 0, set(
-    tone_df.a.values.tolist() + tone_df.x.values.tolist() + tone_df.b.values.tolist())))
-test_wav_list = list(filter(lambda s: s.count("_") != 0, set(
-    tone_df.a.values.tolist() + tone_df.x.values.tolist() + tone_df.b.values.tolist())))
-# %%
+tone_df = pd.read_csv(list_path)[["type", "a", "x", "b"]]\
+    .query("type=='filler'")
+wav_list_set = set(
+    tone_df.a.values.tolist() +
+    tone_df.x.values.tolist() +
+    tone_df.b.values.tolist()
+)
+# _を含むのはH_Lのようなテストデータ
+train_wav_list = list(filter(lambda s: s.count("_") == 0, wav_list_set))
+test_wav_list = list(filter(lambda s: s.count("_") != 0, wav_list_set))
+
 print(f"train wav files are:\n\t{train_wav_list}")
 print(f"test wav files are:\n\t{test_wav_list}")
+
+# %%
 # %%
 # Down-sampling and save
 SR = 16_000
-for wav_i in train_wav_list+test_wav_list:
-    wav_i_path = project_dir/"src/audio/output"/wav_i
-    save_path_i = project_dir / "model/wav" / wav_i
-    y, sr = librosa.load(wav_i_path, sr=None)  # none にすると読んでくれる
+for wav_i in train_wav_list + test_wav_list:
+    y, sr = librosa.load(data_path(wav_i, "original"), sr=None)
     y_16k = librosa.resample(y, sr, SR)
-    sf.write(save_path_i, y_16k, SR)
+    sf.write(data_path(wav_i, "downsample"), y_16k, SR)
+
 # %%
 # 2. Feature Extraction(f0, rms)
-# ここらへん、ハイパラになるかも。
-frame_stride = 0.025  # (25ms)
-# load
 for wav_i in train_wav_list:
-    resample_path_i = project_dir / "model/wav" / wav_i
-    save_path_i = project_dir / "model/wav" / wav_i
-    y, sr = librosa.load(resample_path_i, sr=SR)  # none にすると読んでくれる
-    assert sr == SR
+    frame_stride = 0.025  # (25ms)
+    y, sr = librosa.load(data_path(wav_i, "downsample"), SR)
     hop_length = int(frame_stride*sr)
-    f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz(
-        'C2'), fmax=librosa.note_to_hz('C7'), sr=sr, hop_length=hop_length)
-    f0 = f0.reshape(1, -1)
-    # RMS value for each frame
-    rms = librosa.feature.rms(y=y, hop_length=hop_length)  # 音量の計算
-    feature = np.concatenate([f0, rms])
-    file_name = wav_i.split(".")[0]
-    save_path_i = project_dir / "model/feature" / file_name
-    np.save(save_path_i, feature, allow_pickle=False)
+    f0, _, _ = librosa.pyin(
+        y,
+        fmin=librosa.note_to_hz('C2'),
+        fmax=librosa.note_to_hz('C7'),
+        sr=sr,
+        hop_length=hop_length
+    )
+    rms = librosa.feature.rms(y=y, hop_length=hop_length)
+    feature = np.concatenate([f0.reshape(1, -1), rms])
+    np.save(data_path(wav_i, "feature"), feature, allow_pickle=False)
+
+# %%
+
+
+def data_path(wav_path, data_type):
+    # p.stem
+    # TODO: add test
+    project_dir = Path("../")
+    accepted_types = [
+        "original", "downsample", "feature",
+        "label_base", "label_rle", "label_rle_delta"
+    ]
+    assert data_type in accepted_types
+    data_path_map = {
+        "original": project_dir / "src/audio/output" / wav_path,
+        "downsample": project_dir / "model/wav" / wav_path,
+        "feature": project_dir / "model/feature" / str(wav_i.split(".")[0]+".npy"),
+    }
+    return data_path_map[data_type]
 
 # %%
 # 3. Labeling
-# wavの名前から作成する
-# window関数とパラメータをチューニングしてうまく割る
-def objective(trial):
-    n_correct = 0
-    # params(find_peaksは色々試した)
-    beta = trial.suggest_uniform("beta", 0.5, 2)
-    percentile_lower = trial.suggest_int("percentile_lower", 10, 50)
-    for wav_i in train_wav_list:  # [:1]:
-        file_name = wav_i.split(".")[0]+".npy"
-        arr_i_path = project_dir / "model/feature" / file_name
-        x = np.load(arr_i_path, allow_pickle=False)[1, :]
-        # 境界値が不安定なため、窓がないと1にはならない.
-        window = 1-np.kaiser(len(x), beta=beta)
-        x = x + window
-        _, height = np.percentile(x, [75, percentile_lower])
-        peaks, _ = find_peaks(-x, height=-height,)
-        label_i = wav_i.split("-")[1]
-        n_mora = len(label_i)
-        n_peak = len(peaks)
-        n_split = n_mora-1
-        n_correct += n_split == n_peak
 
+
+def objective(trial):
+    # 境界にあるデータが不安定なため、窓を当ててpeakから除外する。
+    # find_peaksは色々試したが、結果 percentile_lowerによる heightが大事だった
+    # window関数とパラメータをチューニングしてうまく割る
+    n_correct = 0
+    # params
+    beta = trial.suggest_uniform("beta", 0.3, 3)
+    percentile_lower = trial.suggest_int("percentile_lower", 5, 60)
+    th = trial.suggest_uniform("th", -10, -0)
+    dist = trial.suggest_uniform("dist", 1, 20)
+    for wav_i in train_wav_list:  # [:1]:
+        rms = np.load(data_path(wav_i, "feature"), allow_pickle=False)[1, :]
+        window = 1-np.kaiser(len(rms), beta=beta)
+        rms -= window
+        _, height = np.percentile(rms, [75, percentile_lower])
+        peaks, _ = find_peaks(
+            -rms,
+            height=-height,
+            threshold=th,
+            distance=dist,
+        )
+        label_i = wav_i.split("-")[1]
+        n_mora, n_peak = len(label_i), len(peaks)
+        n_split = n_mora - 1
+        n_correct += int(n_split == n_peak)
     return n_correct/len(train_wav_list)
 
 
 study = optuna.create_study(
-    direction="maximize", sampler=optuna.samplers.TPESampler(seed=42))
-study.optimize(objective, n_trials=100)
+    direction="maximize",
+    sampler=optuna.samplers.TPESampler(seed=42)
+)
+study.optimize(objective, n_trials=50)
 # ALL GREEEN
 # %%
 study.best_params
@@ -110,16 +132,16 @@ cond = ["base", "rle", "rle_delta"]
 for wav_i in train_wav_list:
     file_name = wav_i.split(".")[0]
     arr_i_path = project_dir / "model/feature" / f"{file_name}.npy"
-    x = np.load(arr_i_path, allow_pickle=False)[1, :]
-    n_len = len(x)
+    rms = np.load(arr_i_path, allow_pickle=False)[1, :]
+    n_len = len(rms)
     window = 1-np.kaiser(n_len, beta=best_params["beta"])
-    x = x + window
-    _, height = np.percentile(x, [75, best_params["percentile_lower"]])
-    peaks, _ = find_peaks(-x, height=-height,)
+    rms = rms + window
+    _, height = np.percentile(rms, [75, best_params["percentile_lower"]])
+    peaks, _ = find_peaks(-rms, height=-height,)
     # peak に 1 をたてて cumsum して、それをindexにすればいい。
     # Plot
-    plt.plot(peaks, x[peaks], "xr")
-    plt.plot(x)
+    plt.plot(peaks, rms[peaks], "xr")
+    plt.plot(rms)
     plt.plot(window)
     plt.legend(['distance'])
     plt.show()
