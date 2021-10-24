99df52e3382b438df9fbc575b591cff117347580
kishiyamat <kishiyama.t@hotmail.com>
2021-10-24 15:46:40 +0900

    Feature/hsmm (#54)
     実験のセッティング
     モデルの作成と予測 #47
     decodeの動作確認
     ポアソンにする必要はなく、経験的な分布でOKかと #56
     特徴量自体も要因 #50
     実験の要因などをまとめる #51
     パイロット実験
     可視化コード追加

---
+++ model/04_simulation.py
@@ -0,0 +1,64 @@
+# %%
+import pandas as pd
+
+from modeler import Modeler
+from path_manager import PathManager
+
+# %%
+# Setting for experiment
+# TODO: duration の設定などの追加
+if __name__ == "__main__":
+    print("04_simulation.py")
+    # %%
+    setting = PathManager.setting_df
+    setting_dicts = [d.to_dict() for _, d in setting.iterrows()]
+
+    results_list = []
+    for condition_i, setting_i in enumerate(setting_dicts):
+        # DataLoad
+        train_x, train_y, test_x, test_token = PathManager.load_data(
+            **setting_i)
+        # Modeling
+        model = Modeler(**setting_i)
+        model.fit(train_x, train_y)
+        # Experiment
+        for sample_idx, test_token_i in enumerate(test_token):
+            test_token_i = test_token_i.split(".")[0]  # npyを除外
+            phoneme, pitch, speaker = test_token_i.split("-")
+            test_y = model.predict(test_x[sample_idx], visual=False)
+            res = model.to_pattern(test_y)
+            results = {
+                "sample_idx": [sample_idx],
+                "area": [setting_i["area"]],
+                "encoding": [setting_i["encoding"]],
+                "feature": [setting_i["feature"]],
+                "phoneme": [phoneme],
+                "pitch": [pitch],
+                "speaker": [speaker],
+                "res": [res],
+            }
+            results_list.append(pd.DataFrame(results))
+
+    results_df = pd.concat(results_list)
+    results_df.to_csv("artifacts/results.csv")
+
+# %%
+results_df.head()
+combine_lambda = lambda x: '{}+{}+{}'.format(x.area, x.encoding, x.feature)
+results_df["conditions"] = results_df.apply(combine_lambda, axis = 1)
+exp_cond = list(set(results_df.conditions))
+exp_cond.sort()
+
+from plotnine import *
+# まさかの rle+pitch 条件が最も良い結果？
+
+for exp_cond_i in exp_cond:
+    results_df_i = results_df.query(f"conditions == '{exp_cond_i}'") 
+    print(exp_cond_i)
+    gg = (
+        ggplot(results_df_i, aes(x='res'))
+        + facet_grid(".~pitch")
+        + geom_histogram(binwidth=0.5) # specify the binwidth
+        + theme(axis_text_x=element_text(rotation=90, hjust=1))
+    )
+    print(gg)
--- model/path_manager.py
+++ model/path_manager.py
@@ -33,13 +33,20 @@ class PathManager:
             "LLH": ["L2", "L2", "dH1"],
         },
     }
+    accept = {"tokyo": ["HL", "LH", "LHH", "HLL"],
+              "kinki": ["HL", "HH", "LH", "LL", "LLH", "HLL", "HHL", ]}
     tone_df = pd.read_csv(project_dir/"src/list/axb_list.csv")\
         .query("type=='filler'")
+    setting_df = pd.read_csv(project_dir/"model/setting.csv")
 
     @classmethod
     def data_path(cls, data_type, wav_path=""):
+        """
+        wav_path: wav_path じゃなくても良い(どのみち処理するので) 
+        """
         # 参照したいタイプを渡すとパスを返す
         project_dir = cls.project_dir
+        # NOTE: 予測に失敗した時、近畿のLHH拒否が原因かもしれない
         accepted_types = [
             "original", "downsample", "feature", "pitch_delta",
             "label_base", "label_rle", "label_rle_delta",
@@ -71,28 +78,80 @@ class PathManager:
         test_wav_list = list(filter(lambda s: s.count("_") != 0, wav_list_set))
         return train_wav_list, test_wav_list
 
-    def load_data(area, encoding, **kwargs) -> tuple:
-        encoding_methods = ["base", "rle", "rle_delta"]
+    @staticmethod
+    def is_illusory(path) -> bool:
+        """
+        hoge/hoge.x -> false
+        hoge/ho_ge.x -> true
+        """
+        return bool(path.split("/")[-1].count("_"))
+
+    @staticmethod
+    def is_not_illusory(path) -> bool:
+        """
+        hoge/hoge.x -> true
+        hoge/ho_ge.x -> false
+        """
+        return not bool(path.split("/")[-1].count("_"))
+
+    @classmethod
+    def load_data(cls, area, encoding, feature, **kwargs) -> tuple:
+        # check if the settings are just right
         spaces = ["kinki", "tokyo"]
-        assert area in spaces and encoding in encoding_methods
-        project_dir = Path("../")
-        data_path = glob(str(project_dir/"model/feature/*.npy"))
-        train_path = filter(lambda p: p.split(
-            "/")[-1].count("_") == 0, data_path)
-        test_path = filter(lambda p: p.split(
-            "/")[-1].count("_") >= 1, data_path)
+        encoding_methods = ["base", "rle", "rle_delta"]
+        feature_set = ["pitch", "pitch:pitch_delta", "pitch_delta"]
+        assert area in spaces \
+            and encoding in encoding_methods \
+            and feature in feature_set
+
+        # Get tokens for train and test
+        feature_path = glob(str(cls.project_dir/"model/feature/*.npy"))
+        pitch_delta_path = glob(str(cls.project_dir/"model/pitch_delta/*.npy"))
+        # 前処理が適切なら feature と pitch_delta の数が等しい
+        assert len(feature_path) == len(pitch_delta_path)
+
+        # feature_pathからtrainとtestに分ける
+        train_path = filter(cls.is_not_illusory, feature_path)
+        test_path = filter(cls.is_illusory, feature_path)
         train_token = [p.split("/")[-1] for p in train_path]
-        # filter by area
-        # TODO: 予測に失敗した時、近畿のLHH拒否が原因かもしれない
-        accept = {"tokyo": ["HL", "LH", "LHH", "HLL"],
-                  "kinki": ["HL", "HH", "LH", "LL", "LLH", "HLL", "HHL", ]}
+        test_token = [p.split("/")[-1] for p in test_path]
+        # filter by area: token(LLHなど)が条件(kinki--tokyo)を満たすか
         train_token = list(filter(
-            lambda s: s.split("-")[1] in accept[area],
+            lambda s: s.split("-")[1] in cls.accept[area],
             train_token
         ))
-        train_x = [np.load(str(project_dir/"model/feature"/t))
-                   for t in train_token]
+        # Xの組み合わせは3種類ある
+        feature_set = ["pitch", "pitch:pitch_delta", "pitch_delta"]
+        train_x = []
+        for t in train_token:
+            pitch = np.load(cls.data_path("feature", t))[0, :].reshape(1, -1)
+            delta = np.load(cls.data_path("pitch_delta", t))
+            if feature == "pitch":
+                train_x.append(pitch)
+            elif feature == "pitch:pitch_delta":
+                train_x.append(np.concatenate([pitch, delta]))
+            elif feature == "pitch_delta":
+                train_x.append(delta)
+            else:
+                raise NotImplementedError
+
         train_y = [
-            np.load(str(project_dir/f"model/label_{encoding}"/t)) for t in train_token]
-        test_x = [np.load(p) for p in test_path]
-        return train_x, train_y, test_x, None
+            np.load(cls.data_path(f"label_{encoding}", t))
+            for t in train_token
+        ]
+        test_x = []
+        for t in test_token:
+            pitch = np.load(cls.data_path("feature", t))[0, :].reshape(1, -1)
+            delta = np.load(cls.data_path("pitch_delta", t))
+            # testはそのままdecodeできる値にする。
+            # pitch や pitch_delta は GausianHSMM
+            # pitch:pitch_delta は Multivariate の入力になる
+            if feature == "pitch":
+                test_x.append(pitch.flatten())
+            elif feature == "pitch:pitch_delta":
+                test_x.append(np.concatenate([pitch, delta]).T)
+            elif feature == "pitch_delta":
+                test_x.append(delta.flatten())
+            else:
+                raise NotImplementedError
+        return train_x, train_y, test_x, test_token
