669e5499100b42dcf561077895a136d9a590bd5f
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2021-11-12 11:37:39 +0900

    csv の作り方を修正

--- results/make_annotation_list.py
+++ results/make_annotation_list.py
@@ -5,8 +5,7 @@ from pathlib import Path
 
 lines = []
 for path_i in glob.glob("wav/*.wav"):
-    filename = Path(path_i).with_suffix("").stem
-    assert not "." in filename
+    filename = path_i[4:-4]  # wav/ と .wav を省く
     assert not "," in filename
     assert not "\n" in filename
     # ユーザー名が _ を含んでも-1なら問題ない
@@ -14,6 +13,7 @@ for path_i in glob.glob("wav/*.wav"):
     for order in range(1, 4):
         lines.append(",".join([filename, item_id, str(order), "NA"]))
 lines.sort()
+lines
 # %%
 with open("_devoicing_annotation.csv", 'w') as file:  # -> wav
     lines.insert(0, ",".join(["filename", "item_id", "order", "devoiced"]))
