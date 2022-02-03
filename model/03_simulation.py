b3132bfe687bab3b3fcf3da9807fe92c14468fd1
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2022-02-03 14:42:58 +0900

    問題なく動作する

--- model/03_simulation.py
+++ model/03_simulation.py
@@ -44,12 +44,18 @@ for use_semitone, use_duration, use_transition, tokyo_kinki_ratio in list(condit
     print(use_semitone, use_duration, use_transition, tokyo_kinki_ratio)
     X, y = model.df2xy(train_df)
     model.fit(X, y)
+    y_collapsed_list = []
     for _, df_by_stimuli in test_df_3mora.groupby("stimuli"):
         X, _ = model.df2xy(df_by_stimuli)
         X_flatten = np.concatenate(X)  # 実際の入力は区切られていない
         y = model.percept(X_flatten)
+        y_collapsed = tuple(rle.encode(y)[0])
         print("fname:\t", df_by_stimuli.stimuli[0])
         print("pitch:\t", df_by_stimuli.collapsed_pitches[0])
-        print("decode:\t", rle.encode(y))
-
-# %%
+        print("decode:\t", y_collapsed)
+        y_collapsed_list.append(y_collapsed)
+    n_exp = len(y_collapsed_list)
+    n_tokyo = sum([y in model.tokyo_pattern for y in y_collapsed_list ])
+    n_kinki = sum([y in model.kinki_pattern for y in y_collapsed_list ])
+    print(n_tokyo)
+    print(n_kinki)
