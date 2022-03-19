02795967168d594ac3c6812fe75b355aa6205740
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2022-03-20 05:35:33 +0900

    dfづくり完了

--- src/pitch_stats.py
+++ src/pitch_stats.py
@@ -1,18 +1,43 @@
 # %%
+import parselmouth
+import pandas as pd
 import glob
 from pathlib import Path
 
-lines = []
-mora_2 = ["esko", "etsto"]
-mora_3 = ["esuko", "etsuto"]
+# duration for reference
+# duration for reference
+mora_dict = dict(
+    esko="reference",
+    etsto="reference",
+    esuko="distractor",
+    etsuto="distractor"
+)
+mora_dict.items()
+# %%
+# %%
+results = []
+for k, v in mora_dict.items():
+    wavs = glob.glob(f"audio/output/{k}*.wav")
+    for wav in wavs:
+        results.append(
+            pd.DataFrame(
+                dict(
+                    phoneme=[k],
+                    s_type=[v],
+                    fname=[Path(wav).name],
+                    duration=[max(parselmouth.Sound(wav).xs())],
+                )))
+results = pd.concat(results, ignore_index=True)
+# %%
+results
+# %%
+# %%
 
-phoneme_i = "esko"
-wavs = list(filter(lambda s: phoneme_i in s, glob.glob("audio/output/*.wav")))
-
-import parselmouth
 
 print(wavs[0])
 snd = parselmouth.Sound(wavs[0])
+# xのmaxがsになっているので、これの平均と分散を
+# referenceなどごとに見る
 max(snd.xs())
 
 # lines.sort()
